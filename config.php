8a53decb9ab7c72438a562d77b44fb31da0ed075
Charlieman <charlieman.sangma@protonmail.com>
2025-04-19 18:05:22 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2016 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "root";
+// MySQL Database Password
+$sql_db_pass = "";
+// MySQL Database Name
+$sql_db_name = "wowonder151";
+// MySQL Port
+$sql_db_port = "3306";
+
+// Site URL
+$site_url = "http://localhost:81/@www/geeknews.beta_v1.0.1"; // e.g (http://example.com)
+
+$auto_redirect = false;
+
+// Purchase code
+$purchase_code = ""; // Your purchase code, don't give it to anyone.
+?>
